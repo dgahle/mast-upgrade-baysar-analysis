7a9e12c7c298d44995869e0d5bec7b9175303ba9
dgahle <dsgahle1771@outlook.com>
2023-01-10 20:46:33 +0000

    Removed set_path=False in logger and changed logger.debug to .info

--- scripts/simple-c-ii-analysis/main.py
+++ scripts/simple-c-ii-analysis/main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 # Variables
 script_name: str = Path(__file__).name
-logger = get_logger(script_name, set_path=False)
+logger = get_logger(script_name)
 
 
 # Functions and classes
@@ -13,7 +13,7 @@ def main() -> None:
     logger.info('Building parameter posterior distribution')
     logger.info('Evaluate ionisation balance')
     logger.info('Evaluate PECs')
-    logger.debug('Calculate TEC')
+    logger.info('Calculate TEC')
     logger.info('Get C II emission')
     logger.info('Calculate carbon concentration!')
     logger.info('Completed main!')
