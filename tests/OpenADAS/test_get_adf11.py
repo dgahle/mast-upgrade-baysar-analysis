c078cb1876e1006a1625ad7ef0f089fa98d461c2
Daljeet Singh Gahle <47973717+dgahle@users.noreply.github.com>
2023-02-10 17:21:38 +0000

    Renamed TestGetAdf11 tests to be descriptive

--- tests/OpenADAS/test_get_adf11.py
+++ tests/OpenADAS/test_get_adf11.py
@@ -8,19 +8,19 @@ from backend.OpenADAS import get_adf11
 # Functions and classes
 class TestGetAdf11:
 
-    def test_one(self) -> None:
+    def test_H_ACD_yr12(self) -> None:
         adf11: str = get_adf11(element='h', adf11type='acd', year=12)
         assert type(adf11) is str
 
-    def test_two(self) -> None:
+    def test_Li_SCD_93_r(self) -> None:
         adf11: str = get_adf11(element='li', adf11type='scd', year=93, resolved=True)
         assert type(adf11) is str
 
-    def test_three(self) -> None:
+    def test_C_CCD_r(self) -> None:
         adf11: str = get_adf11(element='c', adf11type='ccd', resolved=True)
         assert type(adf11) is str
 
-    def test_four(self) -> None:
+    def test_N_QCD(self) -> None:
         adf11: str = get_adf11(element='n', adf11type='qcd')
         assert type(adf11) is str
 
