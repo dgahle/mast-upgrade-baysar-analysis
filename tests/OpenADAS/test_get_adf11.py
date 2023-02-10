e7c16ef620ebd7ec64b60b04c6d68aeb8d35ea46
Daljeet Singh Gahle <47973717+dgahle@users.noreply.github.com>
2023-02-10 18:35:19 +0000

    Updated get_adf11 tests to include tests for PLT and PRB, as well as correcting the Li test to download a file that exists.

--- tests/OpenADAS/test_get_adf11.py
+++ tests/OpenADAS/test_get_adf11.py
@@ -12,8 +12,8 @@ class TestGetAdf11:
         adf11: str = get_adf11(element='h', adf11type='acd', year=12)
         assert type(adf11) is str
 
-    def test_Li_SCD_93_r(self) -> None:
-        adf11: str = get_adf11(element='li', adf11type='scd', year=93, resolved=True)
+    def test_Li_SCD_96_r(self) -> None:
+        adf11: str = get_adf11(element='li', adf11type='scd', year=96, resolved=True)
         assert type(adf11) is str
 
     def test_C_CCD_r(self) -> None:
@@ -22,6 +22,29 @@ class TestGetAdf11:
 
     def test_N_QCD(self) -> None:
         adf11: str = get_adf11(element='n', adf11type='qcd')
+        assert type(adf11) is str
+
+    def test_Ne_XCD(self) -> None:
+        adf11: str = get_adf11(element='ne', adf11type='xcd')
+        assert type(adf11) is str
+
+
+class TestGetAdf11Power:
+
+    def test_H_PLT_yr12(self) -> None:
+        adf11: str = get_adf11(element='h', adf11type='plt', year=12)
+        assert type(adf11) is str
+
+    def test_H_PLB_yr12(self) -> None:
+        adf11: str = get_adf11(element='h', adf11type='prb', year=12)
+        assert type(adf11) is str
+
+    def test_C_PLT(self) -> None:
+        adf11: str = get_adf11(element='c', adf11type='plt')
+        assert type(adf11) is str
+
+    def test_N_PLB(self) -> None:
+        adf11: str = get_adf11(element='c', adf11type='prb')
         assert type(adf11) is str
 
 
